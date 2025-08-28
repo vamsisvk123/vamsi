1bd9dd73d2e03cb3398252a06814f0acac4fdfb1
vamsisvk123 <vswarna47@gmail.com>
2025-08-28 01:33:26 -0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import Navigation from './components/Navigation/Navigation';
 import Background from './components/Background/Background';
 import Hero from './components/Hero/Hero';
