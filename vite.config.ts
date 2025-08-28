af7d98b08395e6ad9c6bb7b1f53e9d296cddeac8
vamsisvk123 <vswarna47@gmail.com>
2025-08-28 01:18:26 -0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/your-actual-repo-name/', // Replace with YOUR GitHub repo name
+  base: '/vamsi/', // Replace with YOUR GitHub repo name
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
