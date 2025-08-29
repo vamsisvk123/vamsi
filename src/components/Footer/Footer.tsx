59e3e2a9ff74280f03122268557fb3e6ef453d17
vamsisvk123 <vswarna47@gmail.com>
2025-08-29 00:28:36 -0500

    footer changes

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -9,13 +9,13 @@ const Footer: React.FC = () => {
         <div className="footer-content">
           <p>&copy; 2024 Vamsikrishna. Crafted with passion and code.</p>
           <div className="footer-links">
-            <a href="mailto:vamsikrishna@example.com">
+            <a href="mailto:vamsiswarna74@gmail.com">
               <Mail size={18} />
             </a>
-            <a href="https://linkedin.com/in/vamsikrishna">
+            <a href="https://linkedin.com/in/vamsi-krishna-9715b115a/" target="_blank">
               <Linkedin size={18} />
             </a>
-            <a href="https://github.com/vamsikrishna">
+            <a href="https://github.com/vamsisvk123" target="_blank">
               <Github size={18} />
             </a>
           </div>
